9e43f93ee78e7fef1feb2829ccf9afb87c299ddb
Adreonyx Masanes <Developer.Adreonyx@outlook.com>
2023-11-24 03:52:42 -0900

    test: Entity Manager test case

--- main.cpp
+++ main.cpp
@@ -7,5 +7,31 @@
 
 int main()
 {
+    EntityManager test;
+    test.addEntity("Player");
+    test.addEntity("Bullet");
+    test.update();
+    // Entity tag: Player
+    // Entity ID: 1
+    // Entity tag: bullet
+    // Entity ID: 2
+    for(auto& e : test.getEntities())
+    {
+        std::cout << "Entity tag: " << e->getTag() << std::endl;
+        std::cout <<" Entity ID: " << e->getId() << std::endl;
+    }
+    for(auto& e: test.getEntities("Player"))
+    {
+        e->destroy();
+    }
+    test.update();
+    // Entity tag: bullet
+    // Entity ID: 2
+    for(auto& e : test.getEntities())
+    {
+        std::cout << "Entity tag: " << e->getTag() << std::endl;
+        std::cout <<" Entity ID: " << e->getId() << std::endl;
+    }
+
     return 0;
 }
