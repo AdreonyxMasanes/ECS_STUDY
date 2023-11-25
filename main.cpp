bad859a4d37686ffbe03a7fbf27e3ca87d4d1897
Adreonyx Masanes <Developer.Adreonyx@outlook.com>
2023-11-24 22:25:59 -0900

    fix: add main game loop

--- main.cpp
+++ main.cpp
@@ -1,37 +1,10 @@
 // Run Steps g++ -c main.cpp
 // g++ main.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
 // ./sfml-app 
-#include <iostream>
-#include "EntityManager.h"
-
+#include "Game.h"
 
 int main()
 {
-    EntityManager test;
-    test.addEntity("Player");
-    test.addEntity("Bullet");
-    test.update();
-    // Entity tag: Player
-    // Entity ID: 1
-    // Entity tag: bullet
-    // Entity ID: 2
-    for(auto& e : test.getEntities())
-    {
-        std::cout << "Entity tag: " << e->getTag() << std::endl;
-        std::cout <<" Entity ID: " << e->getId() << std::endl;
-    }
-    for(auto& e: test.getEntities("Player"))
-    {
-        e->destroy();
-    }
-    test.update();
-    // Entity tag: bullet
-    // Entity ID: 2
-    for(auto& e : test.getEntities())
-    {
-        std::cout << "Entity tag: " << e->getTag() << std::endl;
-        std::cout <<" Entity ID: " << e->getId() << std::endl;
-    }
-
+    Game g("config.txt");
     return 0;
 }
