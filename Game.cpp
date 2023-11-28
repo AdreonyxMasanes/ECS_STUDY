0d721a7f990518cc21189a635aeddac4291dbb2b
Adreonyx Masanes <Developer.Adreonyx@outlook.com>
2023-11-27 15:35:45 -0900

    chore: remove comment block

--- Game.cpp
+++ Game.cpp
@@ -340,21 +340,3 @@ void Game::run()
     }    
 }
 
-
-// void Game::configLoadTest()
-// {
-//     std::cout << "Enemy Properties: ";
-//     std::cout << m_enemyConfig.SR << " " << m_enemyConfig.CR << " "  << m_enemyConfig.SMAX << " "  << m_enemyConfig.SMAX << " "  << m_enemyConfig.OR
-//     << " " << m_enemyConfig.OG << " "  << m_enemyConfig.OB << " "  << m_enemyConfig.OT << " "  << m_enemyConfig.VMIN << " "  << m_enemyConfig.VMAX
-//     << " " << m_enemyConfig.L << " "  << m_enemyConfig.SI << std::endl;
-
-//     std::cout << "Bullet Properties: ";
-//     std::cout << m_bulletConfig.SR << " " << m_bulletConfig.CR << " "  << m_bulletConfig.S << " "  << m_bulletConfig.FR << " "  << m_bulletConfig.FG
-//     << " " << m_bulletConfig.FB << " "  << m_bulletConfig.OR << " "  << m_bulletConfig.OG << " "  << m_bulletConfig.OB << " "  << m_bulletConfig.OT
-//     << " " << m_bulletConfig.V << " " << m_bulletConfig.L << std::endl;
-
-//     std::cout << "Player Properties: ";
-//     std::cout << m_playerConfig.SR << " " << m_playerConfig.CR << " "  << m_playerConfig.S << " "  << m_playerConfig.FR << " "  << m_playerConfig.FG
-//     << " " << m_playerConfig.FB << " "  << m_playerConfig.OR << " "  << m_playerConfig.OG << " "  << m_playerConfig.OB << " "  << m_playerConfig.OT
-//     << " " << m_playerConfig.V << std::endl;
-// }
